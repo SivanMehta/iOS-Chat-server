4bdd1473fe998833433b9786226f01e3633f2f62
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-11-20 13:18:12 -0500

    syntax error

--- deckLogic.js
+++ deckLogic.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const stem = 'https://deckofcardsapi.com/api/deck/'
 
-function getCards = (callback) => {
+function getCards (callback) {
   request.get(stem + 'new/shuffle/?deck_count=1', (err, res, body) => {
     const deck_id = JSON.parse(body).deck_id
     request.get(stem + deck_id + '/draw/?count=52', (err2, res2, body2) => {
